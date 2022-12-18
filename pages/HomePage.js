e282add52e2ddc041dab647184b0ce8d84367e5d
Faiza <berrichif0717@gmail.com>
2022-12-18 15:53:59 +0100

    add pages

---
+++ pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const  HomePage =()=>{
+  return (
+     <div>
+        <p>react Home</p>
+     </div>
+  )
+}
+
+
+export default HomePage;
---
+++ pages/auth/Login.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const  Login =()=>{
+  return (
+     <div>
+        <p className="text-red-500">Login</p>
+     </div>
+  )
+}
+
+
+export default Login;
